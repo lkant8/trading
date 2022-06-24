e914b5ee8f4950d6715d4926d7ae8621102ced61
Laxmi kant <laxmikant.pal@investwellonline.com>
2022-06-24 17:52:13 +0530

    successfully setup

--- app/src/main/java/com/codeblogs/trading/MainActivity.kt
+++ app/src/main/java/com/codeblogs/trading/MainActivity.kt
@@ -3,11 +3,15 @@ package com.codeblogs.trading
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.material.*
+import androidx.compose.material.SnackbarDefaults.backgroundColor
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.codeblogs.trading.ui.theme.TradingTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,7 +30,18 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Card(
+        modifier = Modifier,
+        shape = MaterialTheme.shapes.medium,
+        backgroundColor = Color.Blue,
+        contentColor = contentColorFor(backgroundColor),
+        border  = null,
+        elevation = 1.dp,
+
+    ) {
+        Text(text = "Hello $name!")
+    }
+
 }
 
 @Preview(showBackground = true)
